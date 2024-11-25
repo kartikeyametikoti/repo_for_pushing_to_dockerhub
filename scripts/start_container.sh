c2724961695a72d8174482cc4d0aab3bde26ae22
kartikeya-1112 <metikotikartikeya1112@gmail.com>
2024-11-25 12:54:29 +0530

    new commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 491085395524.dkr.ecr.us-east-1.amazonaws.com
  
-docker pull 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:latest
+docker pull 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
 
-docker run -d -p 2000:5000 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma 
+docker run -d -p 2000:5000 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
 
