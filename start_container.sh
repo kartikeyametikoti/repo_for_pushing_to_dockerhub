b3dfb6cd08a3f678021d047bf18bb0c07fbafa26
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2024-11-25 22:18:10 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,5 +5,5 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
   
 docker pull 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
 
-docker run -d -p 4000:5000 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
+docker run --name=kartik -d -p 4000:5000 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
 
