3eb84ef984fd78bcaabd3cb7e7bf44776d750f31
kartikeya-1112 <metikotikartikeya1112@gmail.com>
2024-11-25 21:39:41 +0530

    new_commit_stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status
 
-echo "stopped the container"
+# List all running Docker containers and stop & remove them one by one
+sudo docker ps | awk 'NR>1 { print $1 }' | while read -r container_id; do
+    echo "Stopping container: $container_id"
+    sudo docker stop "$container_id"
+    echo "Removing container: $container_id"
+    sudo docker rm "$container_id"
+done
+
+echo "All running containers have been stopped and removed."
+
+
 
